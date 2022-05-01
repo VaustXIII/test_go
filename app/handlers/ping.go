3c5a90972593a3dd48db5c200df26806e15c7e13
VaustXIII <VaustXIII@gmail.com>
2022-05-01 19:44:29 +0300

    upd

--- app/handlers/ping.go
+++ app/handlers/ping.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 )
 
 var Ping = basicHandlerWrapper(handlePing)
 
 func handlePing(responseWriter http.ResponseWriter, request *http.Request) {
-	log.Printf("Handling %s request\n%+v", request.URL, *request)
 	WriteResponse(responseWriter, http.StatusOK, nil)
 }
