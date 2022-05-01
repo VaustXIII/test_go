53cc1440070923ab2517fb2262f8440d23db8033
VaustXIII <VaustXIII@gmail.com>
2022-05-01 21:52:20 +0300

    add tests

--- app/models/models_test.go
+++ app/models/models_test.go
@@ -5,42 +5,96 @@ import (
 	"testing"
 )
 
-func TestLeaderBoardAddAndGet(t *testing.T) {
+func TestLeaderboardAddAndGet(t *testing.T) {
+	var tests = []struct {
+		test_id  string
+		adds     []Client
+		expected []Client
+	}{
+		{"Empty", []Client{}, []Client{}},
+		{"One add", []Client{
+			{Id: 1, Balance: 123},
+		}, []Client{
+			{Id: 1, Balance: 123},
+		}},
+		{"Two adds", []Client{
+			{Id: 1, Balance: 123},
+			{Id: 5, Balance: 987.1},
+		}, []Client{
+			{Id: 5, Balance: 987.1},
+			{Id: 1, Balance: 123},
+		}},
+	}
 
-	t.Run("Empty", func(t *testing.T) {
-		var leaderBoard = NewLeaderboard()
-		var expected = 0
-		var actual = len(*leaderBoard.GetClients())
-		if actual != expected {
-			t.Errorf("got %d, want %d", actual, expected)
-		}
-	})
+	for _, tt := range tests {
+		t.Run(tt.test_id, func(t *testing.T) {
+			var leaderBoard = NewLeaderboard()
+			for _, client := range tt.adds {
+				leaderBoard.AddClient(client.Id, client.Balance)
+			}
+			var actual = leaderBoard.GetClients()
 
-	t.Run("Adding a client", func(t *testing.T) {
-		var leaderBoard = NewLeaderboard()
-		leaderBoard.AddClient(1, 123)
+			if !reflect.DeepEqual(actual, tt.expected) {
+				t.Errorf("actual %v != expected %v", actual, tt.expected)
+			}
+		})
+	}
+}
 
-		var expected = &map[int]Client{
-			1: {Id: 1, Balance: 123},
-		}
-		var actual = leaderBoard.GetClients()
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("got %v, want %v", actual, expected)
-		}
-	})
+func TestLeaderBoardGetClientBalanceNeighbours(t *testing.T) {
+	var tests = []struct {
+		test_id   string
+		adds      []Client
+		client_id int
+		expected  *ClientBalanceNeighbours
+	}{
+		{"Empty", []Client{}, 0, nil},
+		{"Only one", []Client{
+			{0, 123},
+		}, 0, &ClientBalanceNeighbours{Lower_id: -1, Upper_id: -1}},
+		{"No upper", []Client{
+			{1, 123},
+			{34, 98.2},
+		}, 1, &ClientBalanceNeighbours{Lower_id: 34, Upper_id: -1}},
+		{"No lower", []Client{
+			{2, 123},
+			{43, 981.2},
+		}, 2, &ClientBalanceNeighbours{Lower_id: -1, Upper_id: 43}},
+		{"Basic", []Client{
+			{56, 45.34},
+			{43, 981.2},
+			{3, 123},
+		}, 3, &ClientBalanceNeighbours{Lower_id: 56, Upper_id: 43}},
+		{"Longer list", []Client{
+			{4, 1000},
+			{10, 999},
+			{11, 998},
+			{12, 997},
+			{13, 996},
+			{14, 1001},
+			{15, 1002},
+			{16, 1003},
+		}, 4, &ClientBalanceNeighbours{Lower_id: 10, Upper_id: 14}},
+		{"Equal balances", []Client{
+			{4, 1000},
+			{5, 1000},
+			{6, 1000},
+			{12, 99},
+			{13, 9999},
+		}, 4, &ClientBalanceNeighbours{Lower_id: 12, Upper_id: 13}},
+	}
 
-	t.Run("Adding another client", func(t *testing.T) {
-		var leaderBoard = NewLeaderboard()
-		leaderBoard.AddClient(1, 123)
-		leaderBoard.AddClient(5, 987.1)
+	for _, tt := range tests {
+		t.Run(tt.test_id, func(t *testing.T) {
+			var leaderBoard = NewLeaderboard()
+			for _, client := range tt.adds {
+				leaderBoard.AddClient(client.Id, client.Balance)
+			}
+			var actual = leaderBoard.GetClientBalanceNeighbours(tt.client_id)
 
-		var expected = &map[int]Client{
-			1: {Id: 1, Balance: 123},
-			5: {Id: 5, Balance: 987.1},
-		}
-		var actual = leaderBoard.GetClients()
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("got %v, want %v", actual, expected)
-		}
-	})
+			if !reflect.DeepEqual(actual, tt.expected) {
+				t.Errorf("actual %v != expected %v", actual, tt.expected)
+			}
+		})
+	}
 }
